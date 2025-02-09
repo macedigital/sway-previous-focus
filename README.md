f62ddb0695c199816bd9476c02815251565704f9
Matthias Adler <macedigital@users.noreply.github.com>
2025-02-09 22:21:10 +0100

    feat: Convert shell script to golang
    Commit converts the original shell script by wef to using `golang`
    (go-sway).
    
    It's using the same approach of relying solely on Sway's marks system.
    Core assumption is that a mark is globally unique and cannot be re-used
    among multiple windows.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # sway-previous-focus
+
+Static binary that marks previous window to quickly toggle between last recently used ones.
+
+This project uses Sway IPC via [go-sway][1] library.
+
+## Getting started
+
+1. Compile source or put release binary into a folder in `$PATH` (e.g. `~/.local/bin`).
+2. Start script while running in a Sway session (or enable systemd unit for auto re-starts).
+3. Bind key in Sway configuration to jump to window marked with `_prev`.
+
+## Credits
+
+Inspired by <https://gitlab.com/wef/dotfiles/-/blob/c6cd8c0a7e003a2fa2c5f4188e23aad1cc90ce85/bin/i3-track-window-events> and
+discussion at <https://www.reddit.com/r/swaywm/comments/u9txcn/does_anyone_have_interesting_uses_for_sways_marks/>.
+
+[1]: https://github.com/joshuarubin/go-sway
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/joshuarubin/go-sway"
+)
+
+const prevIdentifier string = "_prev"
+const sentinelValue int64 = -1
+
+var prevFocus int64 = sentinelValue
+
+type handler struct {
+	sway.EventHandler
+	sway.Client
+}
+
+func (h handler) Window(ctx context.Context, e sway.WindowEvent) {
+	switch e.Change {
+	case sway.WindowFocus:
+		if e.Container.Focused && e.Container.ID > 0 && *e.Container.Visible {
+			if prevFocus > 0 && prevFocus != e.Container.ID {
+				h.UpdateMark(ctx)
+			}
+			prevFocus = e.Container.ID
+		}
+	case sway.WindowClose:
+		if prevFocus == e.Container.ID {
+			h.UpdateMark(ctx)
+			prevFocus = sentinelValue
+		}
+	}
+}
+
+func (h handler) UpdateMark(ctx context.Context) {
+	cmd := fmt.Sprintf("[con_id=%d] mark --add --toggle %s", prevFocus, prevIdentifier)
+
+	if _, err := h.Client.RunCommand(ctx, cmd); err != nil {
+		log.Printf("Sway command '%s' failed: %s\n", cmd, err)
+	}
+}
+
+func NewHandler(ctx context.Context) (*handler, error) {
+	client, err := sway.New(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	h := &handler{
+		sway.NoOpEventHandler(),
+		client,
+	}
+
+	return h, nil
+}
+
+func main() {
+	ctx := context.Background()
+
+	h, err := NewHandler(ctx)
+	if err != nil {
+		log.Fatalf("Cannot create handler: %s\n", err)
+	}
+
+	if err = sway.Subscribe(ctx, h, sway.EventTypeWindow); err != nil {
+		log.Fatalf("Cannot subscribe to Sway socket: %#v\n", err)
+	}
+}
